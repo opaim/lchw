e8a9f77c7ab354ebb08c8e2ad0fade458ec5f5a1
opaim <odirleipaim01@gmail.com>
2015-03-20 14:14:42 -0300

    commit-20150320

---
+++ ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* warning: this program is wrong on purpose. */
+
+int main()
+{
+	int age = 10;
+	int height = 178;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+	
+}
